c822f1753e025b6cd7ccea23acc7580ca32b59e8
Suhail Singh Bains <bains.singh.suhail@gmail.com>
2021-04-06 13:29:44 -0400

    Day 365

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-i=1
-while [ $i -le 1 ]
+i=365
+while [ $i -ge 1 ]
 do
-    dateTime=`date -v+${i}d +"%Y-%m-%d %H:%M:%S %z"`
+    dateTime=`date -v-${i}d +"%Y-%m-%d %H:%M:%S %z"`
     echo ${dateTime}
     echo ${dateTime}>>log.txt
     echo `git add .`
     echo `git commit -m "Day ${i}" --date "${dateTime}"`
-    i=$(( $i + 1 ))
+    i=$(( $i - 1 ))
 done
 echo `git push origin main --force`
