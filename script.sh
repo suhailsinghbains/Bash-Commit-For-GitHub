d3e74d92217524e790c55af3d2588f31cd045885
Suhail Singh Bains <bains.singh.suhail@gmail.com>
2022-04-06 12:59:14 -0400

    Day

--- script.sh
+++ script.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 x=1
+dateTime=`date`
+echo ${dateTime}
 while [ $x -le 5 ]
 do
     date>>log.txt
     echo `git add .`
-    echo `git commit -m "Day ${i}"`
+    echo `git commit -m "Day ${i}" --date="${dateTime}" `
     x=$(( $x + 1 ))
 done
 echo `git push origin main`
