1406c7d930238e4f6abc0eec464c59bc8bfe9ece
Suhail Singh Bains <bains.singh.suhail@gmail.com>
2022-04-07 13:24:33 -0400

    Day 1

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-i=3
-while [ $i -ge 0 ]
+i=1
+while [ $i -le 11 ]
 do
     dateTime=`date -v+${i}d +"%Y-%m-%d %H:%M:%S %z"`
     echo ${dateTime}
     echo ${dateTime}>>log.txt
     echo `git add .`
     echo `git commit -m "Day ${i}" --date="${dateTime}"`
-    i=$(( $i - 1 ))
+    i=$(( $i + 1 ))
 done
 echo `git push origin main --force`
