26b3f2d2080a2d9eced97105945befe180003a19
Suhail Singh Bains <bains.singh.suhail@gmail.com>
2022-04-09 13:23:57 -0400

    Day 3

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 i=3
 while [ $i -ge 0 ]
 do
-    dateTime=`date -v-${i}d +"%Y-%m-%d %H:%M:%S %z"`
+    dateTime=`date -v+${i}d +"%Y-%m-%d %H:%M:%S %z"`
     echo ${dateTime}
     echo ${dateTime}>>log.txt
     echo `git add .`
